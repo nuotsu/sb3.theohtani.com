f2c622a02e61611dd77bd9f1f73dc9448c8d235b
Mitchell Christ <xzmaru@gmail.com>
2025-04-19 20:31:13 -0700

    minor adj

--- src/ui/game/pitch/Pitch.tsx
+++ src/ui/game/pitch/Pitch.tsx
@@ -1,5 +1,3 @@
-import { cn } from '@/lib/utils'
-
 export default function Pitch({ pitch }: { pitch?: MLB.LivePlayPitch }) {
 	const { details } = pitch ?? {}
 	const { startSpeed } = pitch?.pitchData ?? {}
